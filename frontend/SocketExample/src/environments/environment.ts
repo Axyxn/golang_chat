93ce08b36242d1573869b3df83edcc8376e1ed84
Axyxn <109804418+Axyxn@users.noreply.github.com>
2026-03-01 13:11:08 +0530

    Stop tracking environment files and add to gitignore

--- frontend/SocketExample/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  supabaseUrl: 'https://nqbxmpnfjromgfbfgkbd.supabase.co',
-  supabaseKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5xYnhtcG5manJvbWdmYmZna2JkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MjI4MzE2MSwiZXhwIjoyMDg3ODU5MTYxfQ.veHNr3qzcHQJvsc8EsU4VDaFxClkvzPnS4_TAbckG80',
-  serverUrl: 'https://go-chat-backend-w1tb.onrender.com',
-};
